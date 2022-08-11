4b6bb0e5d5eb0bfedeefa22a8f179d844166fcae
Hlöðver Sigurðsson <hlolli@gmail.com>
2022-08-11 18:45:00 +0200

    terraform init

---
+++ terraform/provider.tf
@@ -0,0 +1,35 @@
+terraform {
+
+  backend "s3" {
+    bucket         = "meta-bundler-terraform"
+    key            = "terraform"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.25.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "state_bucket" {
+  bucket = "meta-bundler-terraform"
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "terraform-lock"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
