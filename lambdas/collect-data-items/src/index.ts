5b82f4d14505ba34e790772f83dbc656b22fe089
Hlöðver Sigurðsson <hlolli@gmail.com>
2022-08-16 17:14:38 +0200

    debug: collect data-items filesystem

--- lambdas/collect-data-items/src/index.ts
+++ lambdas/collect-data-items/src/index.ts
@@ -1,5 +1,13 @@
+import AWS from 'aws-sdk';
+import fs from 'fs';
 import log from './logger.js';
+
+const fsPromises = fs.promises;
 
 export async function handler() {
   log.info('Hello cool!!');
+  log.info(
+    'data-items ready for export',
+    await fsPromises.readdir('/mnt/data-items/unbundled'),
+  );
 }
